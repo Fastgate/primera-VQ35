e344e45ab5c7c63b481b03d5e2bb13771bdb32a0
rampage128 <frederik.wolter@googlemail.com>
2018-12-09 01:46:04 +0100

    Optimize animation code performance

--- ledcontrol.h
+++ ledcontrol.h
@@ -1,58 +1,60 @@
 #ifndef _LEDCONTROL_H_
 #define _LEDCONTROL_H_
 
-//#include <FastLED.h>
+#include <FastLED.h>
 
 class PixelGroupDefinition {
 public:
-	virtual ~PixelGroupDefinition() {};
+	virtual ~PixelGroupDefinition() {
+	}
+	;
 	virtual void set(struct CRGB value) = 0;
 	virtual void setLed(int index, struct CRGB value) = 0;
 	virtual int getLedCount() = 0;
 	virtual struct CRGB * getLed(int index) = 0;
 };
 
-template <int START_INDEX, int NUM_LEDS>
-class PixelGroup : public PixelGroupDefinition {
-  private:
-    CRGB * leds;
-    CFastLED * fastLed;
-    int absoluteLimit = START_INDEX + NUM_LEDS - 1;
+template<int START_INDEX, int NUM_LEDS>
+class PixelGroup: public PixelGroupDefinition {
+private:
+	CRGB * leds;
+	CFastLED * fastLed;
+	int absoluteLimit = START_INDEX + NUM_LEDS - 1;
 
-    void setLedInternal(int index, struct CRGB value) {
-      int absoluteIndex = START_INDEX + index;
-      if (absoluteIndex < START_INDEX || absoluteIndex > absoluteLimit) {
-        // Index out of bounds. Lets just fail silently.
-        return;
-      }
+	void setLedInternal(int index, struct CRGB value) {
+		int absoluteIndex = START_INDEX + index;
+		if (absoluteIndex < START_INDEX || absoluteIndex > absoluteLimit) {
+			// Index out of bounds. Lets just fail silently.
+			return;
+		}
 
-      this->leds[absoluteIndex] = value;
-    }
-  public:
-    PixelGroup(struct CRGB * leds, CFastLED * fastLed) {
-      this->leds = leds;
-      this->fastLed = fastLed;
-    }
-    
-    void set(struct CRGB value) {
-      for (int index = 0; index < NUM_LEDS; index++) {
-        this->setLedInternal(index, value);
-      }
-      this->fastLed->show();
-    }
-    
-    void setLed(int index, struct CRGB value) {
-      this->setLedInternal(index, value);
-      this->fastLed->show();
-    }
+		this->leds[absoluteIndex] = value;
+	}
+public:
+	PixelGroup(struct CRGB * leds, CFastLED * fastLed) {
+		this->leds = leds;
+		this->fastLed = fastLed;
+	}
 
-    int getLedCount() {
-      return NUM_LEDS;
-    }
+	void set(struct CRGB value) {
+		for (int index = 0; index < NUM_LEDS; index++) {
+			this->setLedInternal(index, value);
+		}
+		this->fastLed->show();
+	}
 
-    struct CRGB * getLed(int index) {
-      return &this->leds[START_INDEX + index];
-    }
+	void setLed(int index, struct CRGB value) {
+		this->setLedInternal(index, value);
+		this->fastLed->show();
+	}
+
+	int getLedCount() {
+		return NUM_LEDS;
+	}
+
+	struct CRGB * getLed(int index) {
+		return &this->leds[START_INDEX + index];
+	}
 };
 
 class PixelEffect {
@@ -60,16 +62,17 @@ private:
 	uint8_t numGroups = 0;
 	PixelGroupDefinition * groups[10];
 	unsigned long lastUpdate = 0;
-	unsigned long updateTime = 1000 / (float)60;
+	unsigned long updateTime = 1000 / (float) 60;
 	unsigned int ledCount = 0;
 	CFastLED * fastLed;
 public:
 	PixelEffect(uint8_t frameRate) {
-		this->updateTime = 1000 / (float)frameRate;
+		this->updateTime = 1000 / (float) frameRate;
 		this->fastLed = fastLed;
 	}
 	virtual ~PixelEffect() {
 	}
+	;
 	void addGroup(PixelGroupDefinition * group) {
 		this->groups[this->numGroups] = group;
 		this->numGroups++;
@@ -78,23 +81,24 @@ public:
 	void update(CFastLED * fastLed) {
 		unsigned long deltaTime = millis() - lastUpdate;
 		if (deltaTime >= this->updateTime) {
-			this->beforeFrame(this->ledCount, deltaTime);
-			unsigned int pixelIndex = 0;
-			for (PixelGroupDefinition * group : this->groups) {
-				if (group != NULL) {
-					for (int i = 0; i < group->getLedCount(); i++) {
-						onFrame(group->getLed(i), pixelIndex, this->ledCount,
-								deltaTime);
-						pixelIndex++;
+			if (this->beforeFrame(this->ledCount, deltaTime)) {
+				unsigned int pixelIndex = 0;
+				for (PixelGroupDefinition * group : this->groups) {
+					if (group != NULL) {
+						for (int i = 0; i < group->getLedCount(); i++) {
+							onFrame(group->getLed(i), pixelIndex,
+									this->ledCount, deltaTime);
+							pixelIndex++;
+						}
 					}
 				}
+				fastLed->show();
 			}
 			this->afterFrame(this->ledCount, deltaTime);
-			fastLed->show();
 			this->lastUpdate = millis();
 		}
 	}
-	virtual void beforeFrame(unsigned int pixelCount,
+	virtual bool beforeFrame(unsigned int pixelCount,
 			unsigned long deltaTime) = 0;
 	virtual void onFrame(struct CRGB * pixel, unsigned int pixelIndex,
 			unsigned int pixelCount, unsigned long deltaTime) = 0;
--- ledeffects.h
+++ ledeffects.h
@@ -3,6 +3,35 @@
 
 #include "ledcontrol.h"
 
+void _simple_plot_(Stream &output, byte numData, int16_t data1, int16_t data2,
+		int16_t data3, int16_t data4, int16_t data5, int16_t data6) {
+	uint8_t plotBuffer[16];
+	uint16_t pktSize;
+	uint16_t numDataBytes = numData * sizeof(uint16_t); //Size of data in bytes. Does not include the header and size fields
+
+	plotBuffer[0] = 0xAB;
+	plotBuffer[1] = 0xCD; //SimPlot packet header. Indicates start of data packet
+	plotBuffer[2] = lowByte(numDataBytes);
+	plotBuffer[3] = highByte(numDataBytes);
+	plotBuffer[4] = lowByte(data1);
+	plotBuffer[5] = highByte(data1);
+	plotBuffer[6] = lowByte(data2);
+	plotBuffer[7] = highByte(data2);
+	plotBuffer[8] = lowByte(data3);
+	plotBuffer[9] = highByte(data3);
+	plotBuffer[10] = lowByte(data4);
+	plotBuffer[11] = highByte(data4);
+	plotBuffer[12] = lowByte(data5);
+	plotBuffer[13] = highByte(data5);
+	plotBuffer[14] = lowByte(data6);
+	plotBuffer[15] = highByte(data6);
+
+	pktSize = (numData + 2) * sizeof(uint16_t); //Header bytes + size field bytes + data
+
+	output.write((uint8_t *) plotBuffer, pktSize);
+	//output.println();
+}
+
 class SweepEffect: public PixelEffect {
 private:
 	unsigned int tailLength = 0;
@@ -16,11 +45,13 @@ public:
 		this->color = color;
 		this->speed = speed;
 	}
-	void beforeFrame(unsigned int pixelCount, unsigned long deltaTime) {
+	bool beforeFrame(unsigned int pixelCount, unsigned long deltaTime) {
 		(void)(pixelCount);
-		(void)(deltaTime);
+		this->totalDeltaTime += deltaTime;
+		return this->totalDeltaTime > 1000 / speed;
 	}
 	void afterFrame(unsigned int pixelCount, unsigned long deltaTime) {
+		(void)(deltaTime);
 		if (this->totalDeltaTime > 1000 / speed) {
 			if (this->currentPixel < pixelCount - 1) {
 				this->currentPixel++;
@@ -28,8 +59,7 @@ public:
 				this->currentPixel = 0;
 			}
 			this->totalDeltaTime = 0;
-		} else {
-			this->totalDeltaTime += deltaTime;
+			Serial.println("");
 		}
 	}
 	void onFrame(struct CRGB * pixel, unsigned int pixelIndex,
@@ -40,12 +70,15 @@ public:
 
 		if (pixelIndex >= constrain((long)this->currentPixel - this->tailLength, 0, this->currentPixel)
 				&& pixelIndex <= this->currentPixel) {
-			factor = 1 - ((this->currentPixel - pixelIndex) / (float)this->tailLength);
+			factor = 1 - ((this->currentPixel - pixelIndex) / (float)this->tailLength); //(this->tailLength - (this->currentPixel - pixelIndex)) / (float) this->tailLength;
 		}
 
 		pixel->r = this->color.r * factor;
 		pixel->g = this->color.g * factor;
 		pixel->b = this->color.b * factor;
+
+		Serial.print(pixel->r);
+		Serial.print("	");
 	}
 	void setTailLength(unsigned int tailLength) {
 		this->tailLength = tailLength;
